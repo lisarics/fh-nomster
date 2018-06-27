03fed8eda210e96329234cd1314e9ada36a2a8ec
lisarics <lisa.richardson@gmail.com>
2018-06-27 17:19:48 +0000

    Minor bug fix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,17 +18,29 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
-  private
-
-  def edit
+   def edit
     @place = Place.find(params[:id])
   end
 
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+
+
+  private
+
+ 
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
 
   end
+
+
 
 
 end
