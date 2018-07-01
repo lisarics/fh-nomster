6ee63fe0ae51ab459067e3eed7e93dc1b17c837e
lisarics <lisa.richardson@gmail.com>
2018-07-01 17:37:40 +0000

    Added ratings dropbdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,17 @@ class Comment < ApplicationRecord
 
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    'one star': '1-star',
+    'two stars': '2-stars',
+    'three stars': '3-stars',
+    'four stars': '4-stars',
+    'five stars': '5stars'
+
+    }
+
+    def humanized_rating
+      RATINGS.invert[self.rating]
+    end
 end
