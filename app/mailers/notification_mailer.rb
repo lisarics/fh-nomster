3256139e6c62d33c415bafeb12d49b3992376404
lisarics <lisa.richardson@gmail.com>
2018-07-08 16:28:36 +0000

    Initional setup of ActionMailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,8 @@
 class NotificationMailer < ApplicationMailer
+  default from: "no-replay@nomsterapp.com"
+
+  def comment_added
+    mail(to: "lisa.richardson@gmail.com",
+      subject: "A comment has been added to your place")
+  end
 end
